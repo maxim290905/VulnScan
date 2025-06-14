9437260b798786992c024488e98fccf0779cd1b4
JKorolev <ikorolevpro@gmail.com>
2025-06-14 09:06:03 +0300

    Commit 1

--- src/include/sqlinj.h
+++ src/include/sqlinj.h
@@ -1,10 +1,6 @@
-//
-// Created by olegek on 6/13/25.
-//
-
 #ifndef SQLINJ_H
 #define SQLINJ_H
 
-std::string scan_sqli(const std::string& url);
+void scan_sqli(const std::string& url);
 
 #endif //SQLINJ_H
